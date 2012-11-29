5a838db92c76b39049b0ac81ee7467b58d12e8d0
michal waszkielewicz <michal.waszkielewicz@o2.pl>
2012-11-29 11:30:10 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attendance::Application.config.secret_token = '0a2d54d151e243b2119169ea95df3d169e3225913350662c7014cf37a49883441079549970b52c725e74ff757606780d1a8755a91a4a45823b2f397f98d0c1e5'
